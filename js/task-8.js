204c725808414476a9a4355d6fd764d9f729f239
Pankov_Dmytro <pankovdm@gmail.com>
2020-11-10 22:12:31 +0200

    update2

--- js/task-8.js
+++ js/task-8.js
@@ -1,24 +1,16 @@
 const inpField = document.querySelector('#controls>input');
 
-// console.log(inpField);
-
 const createBtn = document.querySelector('[data-action=render]');
-
-// console.log(createBtn);
 
 const clearBtn = document.querySelector('[data-action=destroy]');
 
 const boxes = document.getElementById('boxes');
-
-// console.log(boxes);
-// console.log(clearBtn);
 
 const randomColor = () => Math.floor(Math.random() * 256);
 
 const boxesArr = [];
 
 const createBoxes = (amount) => {
-
     let size = 30;
     for (let i = 0; i < amount; i += 1) {
         let box = document.createElement('div');
@@ -28,28 +20,22 @@ const createBoxes = (amount) => {
         boxesArr.push(box);
         size += 10;
     }
-    return boxesArr;
-};
-const destroyBoxes = () => { };
-
-createBtn.addEventListener('click', () => {
-
-    const boxesCount = parseInt(inpField.value);
-    // console.log(boxesCount);
-
-    createBoxes(boxesCount);
     boxes.append(...boxesArr);
     inpField.value = '';
-});
+};
 
-clearBtn.addEventListener('click', () => {
+const destroyBoxes = () => {
     inpField.value = '';
     boxesArr.splice(0, boxesArr.length)
     boxes.innerHTML = '';
+};
 
-    // console.log(boxesArr);
-    // console.log( 'clear');
+createBtn.addEventListener('click', () => {
+    const boxesCount = Number(inpField.value);
+    createBoxes(boxesCount);
 });
+
+clearBtn.addEventListener('click', destroyBoxes);
 
 
 
